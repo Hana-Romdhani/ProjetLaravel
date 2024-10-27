90e37e369f03bc59b21763aeabafcfb4360ac392
bilelzouaoui00 <bilelzouaoui00@gmail.com>
2024-10-27 21:16:47 +0100

    fix : navigation between pages

--- resources/views/frontend/layouts/layoutUserWorkspace.blade.php
+++ resources/views/frontend/layouts/layoutUserWorkspace.blade.php
@@ -30,7 +30,7 @@
 
 <body class="bg-white">
 
-<nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg">
         <div class="container px-0">
             <a class="navbar-brand" href="/admin">
                 <div class="d-flex align-items-center mb-0">
@@ -85,18 +85,18 @@
                                 </ul>
                             </div>
                         </div>
-                        <a href="#langaugeModal" class="text-inherit me-2" data-bs-toggle="modal">
+                        <a href="/wishlist" class="text-inherit me-2" title="View Wishlist">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
-                                class="bi bi-globe text-gray-500" viewBox="0 0 16 16">
-                                <path
-                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z" />
+                                class="bi bi-heart text-gray-500" viewBox="0 0 16 16">
+                                <path d="M8 15s-7-4.334-7-8a4 4 0 0 1 8 0 4 4 0 0 1 8 0c0 3.666-7 8-7 8zM3.053 3.053A3.993 3.993 0 0 0 0 7c0 3.159 3.826 6.348 6.566 7.92.206.123.434.19.667.19.233 0 .46-.067.666-.19C12.174 13.348 16 10.159 16 7a3.993 3.993 0 0 0-3.053-3.947c-.594-.123-1.282.208-1.558.65a2.086 2.086 0 0 1-3.778 0c-.276-.442-.964-.773-1.558-.65z" />
                             </svg>
                         </a>
+
                         <a href="pages/sign-in.html" class="btn btn-outline-dark">Login</a>
                         <a href="pages/sign-up.html" class="btn btn-dark d-none d-md-block">Join Now</a>
                     </div>
                 </div>
-                 <div>
+                <div>
                     <button class="navbar-toggler collapsed ms-2" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false"
                         aria-label="Toggle navigation">
@@ -108,53 +108,49 @@
                 </div>
                 <!-- Collapse -->
                 <div class="collapse navbar-collapse" id="navbar-default">
-                <ul class="navbar-nav mt-3 mt-lg-0 mx-xxl-auto">
-                <!-- Module de Gestion des Jardins Urbains -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarJardinUrbain" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Jardins</a>
-                    <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarJardinUrbain">
-                        <li><a class="dropdown-item" href="#">Jardin</a></li>
-                        <li><a class="dropdown-item" href="#">Plantation</a></li>
-                    </ul>
-                </li>
+                    <ul class="navbar-nav mt-3 mt-lg-0 mx-xxl-auto">
+                        <!-- Module de Gestion des Jardins Urbains -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarJardinUrbain" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Jardins</a>
+                            <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarJardinUrbain">
+                                <li><a class="dropdown-item" href="#">Jardin</a></li>
+                                <li><a class="dropdown-item" href="#">Plantation</a></li>
+                            </ul>
+                        </li>
 
-                <!-- Module de Gestion des Ressources -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarRessources" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ressources</a>
-                    <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarRessources">
-                        <li><a class="dropdown-item" href="#">Ressource</a></li>
-                        <li><a class="dropdown-item" href="#">Demande Ressource</a></li>
-                    </ul>
-                </li>
+                        <!-- Module de Gestion des Ressources -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarRessources" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ressources</a>
+                            <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarRessources">
+                                <li><a class="dropdown-item" href="#">Ressource</a></li>
+                                <li><a class="dropdown-item" href="#">Demande Ressource</a></li>
+                            </ul>
+                        </li>
 
-                <!-- Module de Conseils -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarConseils" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Conseils</a>
-                    <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarConseils">
-                        <li><a class="dropdown-item" href="#">Conseil</a></li>
-                        <li><a class="dropdown-item" href="#">Catégorie Conseil</a></li>
-                    </ul>
-                </li>
+                        <!-- Module de Conseils -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarConseils" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Conseils</a>
+                            <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarConseils">
+                                <li><a class="dropdown-item" href="#">Conseil</a></li>
+                                <li><a class="dropdown-item" href="#">Catégorie Conseil</a></li>
+                            </ul>
+                        </li>
 
-                <!-- Module de Gestion des Événements -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarEvenements" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Événements</a>
-                    <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarEvenements">
-                        <li><a class="dropdown-item" href="#">Événement</a></li>
-                        <li><a class="dropdown-item" href="#">Classification</a></li>
-                    </ul>
-                </li>
+                        <!-- Module de Gestion des Événements -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarEvenements" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Événements</a>
+                            <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarEvenements">
+                                <li><a class="dropdown-item" href="#">Événement</a></li>
+                                <li><a class="dropdown-item" href="#">Classification</a></li>
+                            </ul>
+                        </li>
 
-                <!-- Module Catalogue des Plantes -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarCataloguePlantes" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Catalogue des Plantes</a>
-                    <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarCataloguePlantes">
-                        <li><a class="dropdown-item" href="#">Plante</a></li>
-                        <li><a class="dropdown-item" href="#">Catégorie Plante</a></li>
-                    </ul>
-                </li>
+                        <!-- Module Catalogue des Plantes -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="/plants" id="navbarCataloguePlantes">Catalogue des Plantes</a>
+                        </li>
 
-            </ul>
+                    </ul>
 
                 </div>
             </div>
--- resources/views/frontend/layouts/layoutfrontend.blade.php
+++ resources/views/frontend/layouts/layoutfrontend.blade.php
@@ -85,18 +85,19 @@
                                 </ul>
                             </div>
                         </div>
-                        <a href="#langaugeModal" class="text-inherit me-2" data-bs-toggle="modal">
+                        <a href="/wishlist" class="text-inherit me-2" title="View Wishlist">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
-                                class="bi bi-globe text-gray-500" viewBox="0 0 16 16">
-                                <path
-                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z" />
+                                class="bi bi-heart text-gray-500" viewBox="0 0 16 16">
+                                <path d="M8 15s-7-4.334-7-8a4 4 0 0 1 8 0 4 4 0 0 1 8 0c0 3.666-7 8-7 8zM3.053 3.053A3.993 3.993 0 0 0 0 7c0 3.159 3.826 6.348 6.566 7.92.206.123.434.19.667.19.233 0 .46-.067.666-.19C12.174 13.348 16 10.159 16 7a3.993 3.993 0 0 0-3.053-3.947c-.594-.123-1.282.208-1.558.65a2.086 2.086 0 0 1-3.778 0c-.276-.442-.964-.773-1.558-.65z" />
                             </svg>
                         </a>
+
+
                         <a href="/user-workspace" class="btn btn-outline-dark">Login</a>
                         <a href="pages/sign-up.html" class="btn btn-dark d-none d-md-block">Join Now</a>
                     </div>
                 </div>
-                 <div>
+                <div>
                     <button class="navbar-toggler collapsed ms-2" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false"
                         aria-label="Toggle navigation">
@@ -108,49 +109,49 @@
                 </div>
                 <!-- Collapse -->
                 <div class="collapse navbar-collapse" id="navbar-default">
-                <ul class="navbar-nav mt-3 mt-lg-0 mx-xxl-auto">
-                <!-- Module de Gestion des Jardins Urbains -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarJardinUrbain" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Jardins</a>
-                    <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarJardinUrbain">
-                        <li><a class="dropdown-item" href={{ route('frontend.jardin.jardin') }}>>Jardin</a></li>
-                        <li><a class="dropdown-item" href="#">Plantation</a></li>
-                    </ul>
-                </li>
+                    <ul class="navbar-nav mt-3 mt-lg-0 mx-xxl-auto">
+                        <!-- Module de Gestion des Jardins Urbains -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarJardinUrbain" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Jardins</a>
+                            <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarJardinUrbain">
+                                <li><a class="dropdown-item" href={{ route('frontend.jardin.jardin') }}>>Jardin</a></li>
+                                <li><a class="dropdown-item" href="#">Plantation</a></li>
+                            </ul>
+                        </li>
 
-                <!-- Module de Gestion des Ressources -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarRessources" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ressources</a>
-                    <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarRessources">
-                        <li><a class="dropdown-item" href="#">Ressource</a></li>
-                        <li><a class="dropdown-item" href="#">Demande Ressource</a></li>
-                    </ul>
-                </li>
+                        <!-- Module de Gestion des Ressources -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarRessources" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ressources</a>
+                            <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarRessources">
+                                <li><a class="dropdown-item" href="#">Ressource</a></li>
+                                <li><a class="dropdown-item" href="#">Demande Ressource</a></li>
+                            </ul>
+                        </li>
 
-                <!-- Module de Conseils -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarConseils" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Conseils</a>
-                    <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarConseils">
-                        <li><a class="dropdown-item" href="#">Conseil</a></li>
-                        <li><a class="dropdown-item" href="#">Catégorie Conseil</a></li>
-                    </ul>
-                </li>
+                        <!-- Module de Conseils -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarConseils" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Conseils</a>
+                            <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarConseils">
+                                <li><a class="dropdown-item" href="#">Conseil</a></li>
+                                <li><a class="dropdown-item" href="#">Catégorie Conseil</a></li>
+                            </ul>
+                        </li>
 
-                <!-- Module de Gestion des Événements -->
-                <li class="nav-item dropdown">
-                    <li><a class="dropdown-item" href="{{ route('frontend.evenement.index') }}" id="navbarEvenements" aria-haspopup="true" aria-expanded="false">Evénements</a></li>
-                    <!-- <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarEvenements">
+                        <!-- Module de Gestion des Événements -->
+                        <li class="nav-item dropdown">
+                        <li><a class="dropdown-item" href="{{ route('frontend.evenement.index') }}" id="navbarEvenements" aria-haspopup="true" aria-expanded="false">Evénements</a></li>
+                        <!-- <ul class="dropdown-menu dropdown-menu-arrow" aria-labelledby="navbarEvenements">
                         <li><a class="dropdown-item" href="{{ route('frontend.evenement.index') }}">Events</a></li>
                         <li><a class="dropdown-item" href="#">Classification</a></li>
                     </ul> -->
-                </li>
+                        </li>
 
-                <!-- Module Catalogue des Plantes -->
-                <li class="nav-item">
-                    <a class="nav-link" href="/plants" id="navbarCataloguePlantes" >Catalogue des Plantes</a>
-                </li>
+                        <!-- Module Catalogue des Plantes -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="/plants" id="navbarCataloguePlantes">Catalogue des Plantes</a>
+                        </li>
 
-            </ul>
+                    </ul>
 
                 </div>
             </div>
@@ -219,20 +220,20 @@
             <div class="row gy-6 gy-xl-0 pb-8">
                 <div class="col-xl-3 col-lg-12 col-md-6 col-12">
                     <div class="d-flex flex-column gap-4">
-                    <div class="d-flex align-items-center mb-0">
-                    <img src="../assets/images/brand/logo/icons8-protea-flower-64.png" class="d-inline" alt="logo-icon" />
-                    <div class="ms-2">
-                        <h1 class="mb-0 fw-bold d-inline">
-                            <span class="mb-0 text-secondary">Green</span>
-                            <span class="mb-0 text-success">
-                                <span>Link</span>
-                            </span>
-                        </h1>
-                        <small class="fst-italic text-muted d-block">
-                            <i class="bi bi-hash text-success"></i>For a Green Life
-                        </small>
-                    </div>
-                </div>
+                        <div class="d-flex align-items-center mb-0">
+                            <img src="../assets/images/brand/logo/icons8-protea-flower-64.png" class="d-inline" alt="logo-icon" />
+                            <div class="ms-2">
+                                <h1 class="mb-0 fw-bold d-inline">
+                                    <span class="mb-0 text-secondary">Green</span>
+                                    <span class="mb-0 text-success">
+                                        <span>Link</span>
+                                    </span>
+                                </h1>
+                                <small class="fst-italic text-muted d-block">
+                                    <i class="bi bi-hash text-success"></i>For a Green Life
+                                </small>
+                            </div>
+                        </div>
                         <p class="mb-0">Nascetur nibh feugiat vulputate urna mauris tincidunt porttitor ultricies. Et dis augue praesent congue.</p>
                         <div class="d-flex gap-2">
                             <a href="#langaugeModal" class="btn btn-outline-secondary" data-bs-toggle="modal">
